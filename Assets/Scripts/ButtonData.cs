ecd415a83355afca7390bec24cb3527d30850094
Vanessa <brodobaggins5@gmail.com>
2020-02-01 15:48:33 -0700

    Last of the changes
    Should be the remaining changes

---
+++ Assets/Scripts/ButtonData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonData : MonoBehaviour
+{
+    public Canvas canvas;
+    public Text detailsText;
+    public string detailsNewText;
+    public void ChangeText(string newText)
+    {
+        detailsText.text = newText;
+    }
+}
---
+++ Assets/Scripts/Tooltips.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tooltips : MonoBehaviour
+{
+    public Text text;   // text object to change, for displaying the tooltip
+    // Start is called before the first frame update
+
+    // Sets the position of the textbox to just above the mouse position
+    void Update()
+    {
+        transform.position = Input.mousePosition + new Vector3(0, 65, 0);
+    }
+    // Sets the text of the tooltip textbox to a new string
+    public void ChangeTooltipText(string newText)
+    {
+        text.text = newText;
+    }
+
+    // reverts the position of the tooltip so it does not interfere with another viewing of it
+    public void RevertPosition()
+    {
+        transform.position = new Vector3(-700, 0, 0);
+    }
+}
