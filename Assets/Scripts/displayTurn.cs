7cfdb54cce53629f9ab0b407ad31af3da0fd2963
Vanessa <brodobaggins5@gmail.com>
2020-02-01 16:41:46 -0700

    CleanUp
    Pushing all the material before I download other material because it's being dumb

---
+++ Assets/Scripts/displayTurn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class displayTurn : MonoBehaviour
+{
+
+    public float turnNumber = 0;
+
+    private Text turn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        turn = GetComponent<Text>();
+    }
+
+    public void increment()
+    {
+        turnNumber++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        turn.text = "Turn: " + turnNumber;
+    }
+}
