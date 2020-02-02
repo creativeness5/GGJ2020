1157044c102788dcb6686f2ff0cd6572bb99476c
Vanessa <brodobaggins5@gmail.com>
2020-02-01 22:14:28 -0700

    Scavenger Code
    Added a function that upon collision with material, will add 2 material, on collision with the dome will subtract 2 material, just need to add code to the material and dome to accept these values

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    
+   
+    public int[] itemSlots = new int[2];
+
+    private PickItem detectedItem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+}
--- Assets/Scripts/Scavenger.cs
+++ Assets/Scripts/Scavenger.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class Scavenger : MonoBehaviour
 {
 
-
+    private Rigidbody rb;
+    
+    public int count;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        count = 0;
     }
 
     // Update is called once per frame
@@ -18,4 +21,19 @@ public class Scavenger : MonoBehaviour
     {
         
     }
+
+    void OnCollisionEnter(Collision colli)
+    {
+        if (colli.gameObject.CompareTag("Material"))
+        {
+            count = count + 2;
+        }
+
+        if (colli.gameObject.CompareTag("Dome"))
+        {
+            count = count - 2;
+        }
+    }
+
+    
 }
