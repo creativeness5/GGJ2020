bf44a357d886f6573fa734e83976f26119fc7f01
aarontkrieg <56848457+aarontkrieg@users.noreply.github.com>
2020-02-01 17:11:41 -0700

    Add files via upload

---
+++ Assets/Scripts/Dictionary.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dictionary : MonoBehaviour
+{
+    public Dictionary<string, string[]> recipes = new Dictionary<string, string[]>()
+    {
+        {"tree", new string[] {"organic", "organic"}},
+        {"animal", new string[] {"organic", "organic", "organic"}},
+    };
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
